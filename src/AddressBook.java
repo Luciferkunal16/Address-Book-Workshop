beb584e71910850fa38dbbc3e1f51c40db16f61a
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-27 20:09:15 +0530

    Added UC3 and UC4 : Edit and Delete Person

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-    List<ContactPerson> listOfcontact= new ArrayList<ContactPerson>();
-    public  void addContacts() {
+    List<ContactPerson> listOfcontact = new ArrayList<ContactPerson>();
+    Scanner inp = new Scanner(System.in);
+
+    public void addContacts() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Name :");
         String firstName = sc.next();
@@ -22,12 +25,89 @@ public class AddressBook {
         long phoneNumber = sc.nextLong();
         System.out.println("Enter The Email :");
         String email = sc.next();
-        ContactPerson contactPerson=new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email);
+        ContactPerson contactPerson = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
         listOfcontact.add(contactPerson);
 
 
     }
-    void viewContacts(){
+
+    void viewContacts() {
         System.out.println(listOfcontact);
+    }
+
+    public void editPerson() {
+
+        System.out.println("Enter the first name:");
+        String firstName = inp.next();
+        Iterator<ContactPerson> iterator = listOfcontact.listIterator();
+
+        while (iterator.hasNext()) {
+
+            ContactPerson person = iterator.next();
+
+            if (firstName.equals(person.getFirstName())) {
+
+
+                System.out.println("\nChoose the attribute you want to change:");
+                System.out.println("1.Last Name\n2.Phone Number\n3.Address\n4.City\n5.State\n6.ZipCode\n Email");
+                int choice = inp.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter the correct Last Name :");
+                        String lastName = inp.next();
+                        person.setLastName(lastName);
+                        break;
+                    case 2:
+                        System.out.println("Enter the correct Phone Number :");
+                        long phoneNumber = inp.nextLong();
+                        person.setPhoneNumber(phoneNumber);
+                        break;
+                    case 3:
+                        System.out.println("Enter Address :");
+                        String address = inp.next();
+                        person.setAddress(address);
+                        break;
+                    case 4:
+                        System.out.println("Enter the correct City :");
+                        String city = inp.next();
+                        person.setCity(city);
+                        break;
+                    case 5:
+                        System.out.println("Enter the correct State :");
+                        String state = inp.next();
+                        person.setState(state);
+                        break;
+                    case 6:
+                        System.out.println("Enter the correct ZipCode :");
+                        int zip = inp.nextInt();
+                        person.setZip(zip);
+                        break;
+                    case 7:
+                        System.out.println("Enter The Correct Email");
+                        String email = inp.next();
+                        person.setEmail(email);
+                }
+
+            }
+        }
+
+    }
+
+    public void deletePerson() {
+
+        System.out.println("Enter the first name of the person to be deleted");
+        String firstName = inp.next();
+        Iterator<ContactPerson> iterator = listOfcontact.listIterator();
+
+        while (iterator.hasNext()) {
+
+            ContactPerson person = iterator.next();
+
+            if (firstName.equals(person.getFirstName())) {
+                listOfcontact.remove(person);
+                return;
+            }
+        }
     }
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -8,12 +8,21 @@ public class AddressBookMain {
         do {
             System.out.println("1)Add Person in Addressbook");
             System.out.println("2)View Contact Person");
+            System.out.println("3)Edit Contact");
+            System.out.println("4)Delete Person");
             choice=inp.nextInt();
             if(choice==1){
                 obj.addContacts();
             }
             else if(choice==2){
                 obj.viewContacts();
+            }
+            else if(choice ==3){
+                obj.editPerson();
+
+            }
+            else if(choice==4){
+                obj.deletePerson();
             }
         }while(choice!=3);
     }
